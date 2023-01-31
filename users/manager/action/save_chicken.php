297d90286d93f17dd897fe4499cd8eb2a102bc29
jayson <jayson.ascencionx@gmail.com>
2023-01-31 23:17:21 +0800

    tooltip update: 'archive record' to 'delete'

--- users/manager/action/save_chicken.php
+++ users/manager/action/save_chicken.php
@@ -128,135 +128,135 @@ try{
                // Attempt to execute the prepared statement
                if($stmt->execute())
                {    
-                    $chickenAge = $age;
-                    $dosage = 1;
-                    $status = 'pending';
-                    $newnote = '*This schedule is auto-generated';
-                    $scheduleType = 'vaccination';
-                    // Retrieve the chickenbatchid base on the submitted coopnumber for a particular user
-                    $getBatchId = "SELECT chickenBatch_ID FROM chickenproduction WHERE coopNumber = $coopNumber";
-                    $stmt = $conn->prepare($getBatchId);
-                    $stmt->execute();
-                    //store the chickenbatchid here
-                    $BatchID = $stmt->fetch(PDO::FETCH_ASSOC);
+                    // $chickenAge = $age;
+                    // $dosage = 1;
+                    // $status = 'pending';
+                    // $newnote = '*This schedule is auto-generated';
+                    // $scheduleType = 'vaccination';
+                    // // Retrieve the chickenbatchid base on the submitted coopnumber for a particular user
+                    // $getBatchId = "SELECT chickenBatch_ID FROM chickenproduction WHERE coopNumber = $coopNumber";
+                    // $stmt = $conn->prepare($getBatchId);
+                    // $stmt->execute();
+                    // //store the chickenbatchid here
+                    // $BatchID = $stmt->fetch(PDO::FETCH_ASSOC);
 
-                    // Initialize the counter variable to be used in accessing user id
-                    $counter = 0;
+                    // // Initialize the counter variable to be used in accessing user id
+                    // $counter = 0;
 
-                    // Initialize the $stmt variable to null
-                    $stmt = null;
+                    // // Initialize the $stmt variable to null
+                    // $stmt = null;
 
-                    // Retrieve the user IDs from the users table where role is employee
-                    $userstmt = $conn->query("SELECT user_ID FROM users WHERE role = 3");
-                    $user_ids = $userstmt->fetchAll(PDO::FETCH_COLUMN);
+                    // // Retrieve the user IDs from the users table where role is employee
+                    // $userstmt = $conn->query("SELECT user_ID FROM users WHERE role = 3");
+                    // $user_ids = $userstmt->fetchAll(PDO::FETCH_COLUMN);
                     
-                    // set the stmt according to the age
-                    if ($age == 0) {
-                        // select all rows from the generateschedules
-                        $stmt = $conn->query("SELECT * FROM generateschedules");
+                    // // set the stmt according to the age
+                    // if ($age == 0) {
+                    //     // select all rows from the generateschedules
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules");
 
-                    } 
-                    else if ($chickenAge > 0 && $chickenAge <= 7) {
-                        // select all rows from the generateschedules except the first 3 rows and will return 5 rows
-                        $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 5 OFFSET 3");
-                    } 
-                    else if ($chickenAge > 7 && $chickenAge <= 21) {
-                        // select all rows from the generateschedules except the first 4 rows and will return 4 rows
-                        $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 4 OFFSET 4");
-                    } 
-                    else if ($chickenAge > 21 && $chickenAge <= 27) {
-                        // select all rows from the generateschedules except the first 5 rows and will return 3 rows
-                        $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 3 OFFSET 5");
-                    } 
-                    else if ($chickenAge > 27 && $chickenAge <= 43) {
-                        // select all rows from the generateschedules except the first 6 rows and will return 2 rows
-                        $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 2 OFFSET 6");
-                    } 
-                    else if ($chickenAge > 43 && $chickenAge <= 60) {
-                        // select all rows from the generateschedules except the first 7 rows and will return 1 rows
-                        $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 1 OFFSET 7");
-                    } else {
-                        $_SESSION['status'] = "Chicken Batch Added Successfully."; 
-                        // Redirect the user to the success page
-                        header("Location: chicken_production.php");
-                        exit;
-                    }
+                    // } 
+                    // else if ($chickenAge > 0 && $chickenAge <= 7) {
+                    //     // select all rows from the generateschedules except the first 3 rows and will return 5 rows
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 5 OFFSET 3");
+                    // } 
+                    // else if ($chickenAge > 7 && $chickenAge <= 21) {
+                    //     // select all rows from the generateschedules except the first 4 rows and will return 4 rows
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 4 OFFSET 4");
+                    // } 
+                    // else if ($chickenAge > 21 && $chickenAge <= 27) {
+                    //     // select all rows from the generateschedules except the first 5 rows and will return 3 rows
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 3 OFFSET 5");
+                    // } 
+                    // else if ($chickenAge > 27 && $chickenAge <= 43) {
+                    //     // select all rows from the generateschedules except the first 6 rows and will return 2 rows
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 2 OFFSET 6");
+                    // } 
+                    // else if ($chickenAge > 43 && $chickenAge <= 60) {
+                    //     // select all rows from the generateschedules except the first 7 rows and will return 1 rows
+                    //     $stmt = $conn->query("SELECT * FROM generateschedules LIMIT 1 OFFSET 7");
+                    // } else {
+                    //     $_SESSION['status'] = "Chicken Batch Added Successfully."; 
+                    //     // Redirect the user to the success page
+                    //     header("Location: chicken_production.php");
+                    //     exit;
+                    // }
 
-                    //if the age of the chicken is not 0, the age will be set to 0 in the first iteration
-                    $is_first_iteration = true;
+                    // //if the age of the chicken is not 0, the age will be set to 0 in the first iteration
+                    // $is_first_iteration = true;
 
-                    // Loop through the rows and insert them into the schedules table
-                    while ($row = $stmt->fetch()) {
-                        //if the age is 0, the age will be set to zero always
-                        if ($chickenAge == 0) {
-                            $age = 0;
-                        } else {
-                            //refactored version If it is the first iteration and the chicken age is 7, 21, 27, 43, or 60, then set the age to 0.
-                            // Otherwise, set the age to the absolute difference between the chicken age and the age in the current row of the generateschedules table.
-                            // Set $is_first_iteration to false to prevent this code block from being executed on subsequent iterations.
-                            if ($is_first_iteration && in_array($chickenAge, [7, 21, 27, 43, 60])) {
-                                $age = 0;
-                              } else {
-                                $age = abs($chickenAge - $row['age']);
-                              }
-                              $is_first_iteration = false;
+                    // // Loop through the rows and insert them into the schedules table
+                    // while ($row = $stmt->fetch()) {
+                    //     //if the age is 0, the age will be set to zero always
+                    //     if ($chickenAge == 0) {
+                    //         $age = 0;
+                    //     } else {
+                    //         //refactored version If it is the first iteration and the chicken age is 7, 21, 27, 43, or 60, then set the age to 0.
+                    //         // Otherwise, set the age to the absolute difference between the chicken age and the age in the current row of the generateschedules table.
+                    //         // Set $is_first_iteration to false to prevent this code block from being executed on subsequent iterations.
+                    //         if ($is_first_iteration && in_array($chickenAge, [7, 21, 27, 43, 60])) {
+                    //             $age = 0;
+                    //           } else {
+                    //             $age = abs($chickenAge - $row['age']);
+                    //           }
+                    //           $is_first_iteration = false;
 
-                            //  old code
-                            // if ($is_first_iteration){
-                            //     if($chickenAge == 7 || $chickenAge == 21 || $chickenAge == 27 || $chickenAge == 43 || $chickenAge == 60){
-                            //         $age = 0; //if the age of the chicken is not zero, this will set the age to 0 for the first iteration so that the sched will be set on the same day.
-                            //         $is_first_iteration = false;
-                            //     }else{
-                            //         $is_first_iteration = false;
-                            //     }
-                            // } else {
-                            //     if($chickenAge > $row['age']){
-                            //         $newAge = $chickenAge - $row['age'];
-                            //     }else{
-                            //         $newAge = $row['age'] - $chickenAge;
-                            //     }                                
-                            //     $age = $newAge;
-                            // }
-                        }
+                    //         //  old code
+                    //         // if ($is_first_iteration){
+                    //         //     if($chickenAge == 7 || $chickenAge == 21 || $chickenAge == 27 || $chickenAge == 43 || $chickenAge == 60){
+                    //         //         $age = 0; //if the age of the chicken is not zero, this will set the age to 0 for the first iteration so that the sched will be set on the same day.
+                    //         //         $is_first_iteration = false;
+                    //         //     }else{
+                    //         //         $is_first_iteration = false;
+                    //         //     }
+                    //         // } else {
+                    //         //     if($chickenAge > $row['age']){
+                    //         //         $newAge = $chickenAge - $row['age'];
+                    //         //     }else{
+                    //         //         $newAge = $row['age'] - $chickenAge;
+                    //         //     }                                
+                    //         //     $age = $newAge;
+                    //         // }
+                    //     }
                         
 
-                        $administrationSched = new DateTime('now'); // create todays date
-                        $administrationSched->add(new DateInterval('P'.$age.'D')); // add the age to the date
-                        $user_id = $user_ids[$counter % count($user_ids)]; 
+                    //     $administrationSched = new DateTime('now'); // create todays date
+                    //     $administrationSched->add(new DateInterval('P'.$age.'D')); // add the age to the date
+                    //     $user_id = $user_ids[$counter % count($user_ids)]; 
 
-                        // Prepare an insert statement
-                        $sql = "INSERT INTO schedules (scheduleType, chickenBatch_ID, coopNumber, medicine_ID, medicineName, methodType, dosage, numberHeads, administrationSched, administeredBy, status, notes) VALUES (:scheduleType, :BatchID, :coopNumber, :medicine_ID, :medicineName, :method, :dosage, :inStock, :administrationSched, :user_id, :status, :newnote)";
-                        $insert_stmt = $conn->prepare($sql);
+                    //     // Prepare an insert statement
+                    //     $sql = "INSERT INTO schedules (scheduleType, chickenBatch_ID, coopNumber, medicine_ID, medicineName, methodType, dosage, numberHeads, administrationSched, administeredBy, status, notes) VALUES (:scheduleType, :BatchID, :coopNumber, :medicine_ID, :medicineName, :method, :dosage, :inStock, :administrationSched, :user_id, :status, :newnote)";
+                    //     $insert_stmt = $conn->prepare($sql);
                     
-                        // Set parameters $param_numberHeads = $inStock;
-                        $param_scheduleType = $scheduleType;
-                        $param_BatchID = $BatchID['chickenBatch_ID'];
-                        $param_coopNumber = $coopNumber;
-                        $param_dosage = $dosage;
-                        $param_numberHeads = $inStock;
-                        $param_administrationSched = $administrationSched->format('Y-m-d');
-                        $param_user_id = $user_id;
-                        $param_status = $status;
-                        $param_newnote = $newnote;
+                    //     // Set parameters $param_numberHeads = $inStock;
+                    //     $param_scheduleType = $scheduleType;
+                    //     $param_BatchID = $BatchID['chickenBatch_ID'];
+                    //     $param_coopNumber = $coopNumber;
+                    //     $param_dosage = $dosage;
+                    //     $param_numberHeads = $inStock;
+                    //     $param_administrationSched = $administrationSched->format('Y-m-d');
+                    //     $param_user_id = $user_id;
+                    //     $param_status = $status;
+                    //     $param_newnote = $newnote;
 
-                        // Bind variables to the prepared statement as parameters
-                        $insert_stmt->bindParam(":scheduleType", $param_scheduleType, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":BatchID", $param_BatchID, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":coopNumber", $param_coopNumber, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":medicine_ID", $row['medicine_ID'], PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":medicineName", $row['medicineName'], PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":method", $row['method'], PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":dosage", $param_dosage, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":inStock", $param_inStock, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":administrationSched", $param_administrationSched, PDO::PARAM_STR); //
-                        $insert_stmt->bindParam(":user_id", $param_user_id, PDO::PARAM_STR); //
-                        $insert_stmt->bindParam(":status", $param_status, PDO::PARAM_STR);
-                        $insert_stmt->bindParam(":newnote", $param_newnote, PDO::PARAM_STR);
+                    //     // Bind variables to the prepared statement as parameters
+                    //     $insert_stmt->bindParam(":scheduleType", $param_scheduleType, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":BatchID", $param_BatchID, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":coopNumber", $param_coopNumber, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":medicine_ID", $row['medicine_ID'], PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":medicineName", $row['medicineName'], PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":method", $row['method'], PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":dosage", $param_dosage, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":inStock", $param_inStock, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":administrationSched", $param_administrationSched, PDO::PARAM_STR); //
+                    //     $insert_stmt->bindParam(":user_id", $param_user_id, PDO::PARAM_STR); //
+                    //     $insert_stmt->bindParam(":status", $param_status, PDO::PARAM_STR);
+                    //     $insert_stmt->bindParam(":newnote", $param_newnote, PDO::PARAM_STR);
                     
-                        $counter++;
-                        // Execute the prepared statement
-                        $insert_stmt->execute();
-                    }
+                    //     $counter++;
+                    //     // Execute the prepared statement
+                    //     $insert_stmt->execute();
+                    // }
 
                     // Close the connection
                     $conn = null;

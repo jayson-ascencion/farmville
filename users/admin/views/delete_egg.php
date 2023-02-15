ff1d06aa7f650b3834ec1d1b3f63518753fc49fa
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-02-15 10:49:39 +0800

    cancel button redirect - updated

--- users/admin/views/delete_egg.php
+++ users/admin/views/delete_egg.php
@@ -133,7 +133,7 @@
                         
                         <div class="card-footer w-100 border d-flex justify-content-between">
                             <div class="w-100 m-1">
-                                <a class="small btn btn-outline-secondary fw-bold w-100" href="./chicken_production.php">
+                                <a class="small btn btn-outline-secondary fw-bold w-100" href="./egg_production.php">
                                     Cancel
                                 </a> 
                             </div>
--- users/admin/views/delete_medicine.php
+++ users/admin/views/delete_medicine.php
@@ -144,7 +144,7 @@
                     <input type="hidden" name="id" value="<?php echo $id; ?>"/>
                     <div class="card-footer w-100 border d-flex justify-content-between">
                         <div class="w-100 m-1">
-                            <a class="small btn btn-outline-secondary fw-bold w-100" href="./chicken_production.php">
+                            <a class="small btn btn-outline-secondary fw-bold w-100" href="./medicines.php">
                                 Cancel
                             </a> 
                         </div>

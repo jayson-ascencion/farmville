19ea5634c12b020a5554bad0fc96b0e335ef31f3
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-03-13 14:33:14 +0800

    tooltip modified

--- users/admin/views/query/schedules.php
+++ users/admin/views/query/schedules.php
@@ -55,7 +55,7 @@
                         }
                         echo "<td>";
                         //update icon/button
-                        echo '<a href="restore_schedules.php?id='. $row['administration_ID'] .'" class="m-1 text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="Edit Record">
+                        echo '<a href="restore_schedules.php?id='. $row['administration_ID'] .'" class="m-1 text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="Restore">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
                         <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>

92bf42e91dabac809e81cbcba7c544f3938d75da
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-04-17 15:02:28 +0800

    removed the indicator for the quantity

--- users/manager/views/query/egg_production_records.php
+++ users/manager/views/query/egg_production_records.php
@@ -44,20 +44,20 @@
 
                     //store the calculated days, %r -> used to include sign and if the number is positive it will be empty, %a -> total number of days as a result from the date_diff https://www.php.net/manual/en/dateinterval.format.php
                    // $days = $diff->format('%r%a');
-
                     echo "<tr>";
                         echo "<td>" . $row['eggBatch_ID'] . "</td>";
                         echo "<td>" . $row['eggSize'] . "</td>";
-                        if($row['quantity'] < 10){
-                            echo "<td>" . $row['quantity'] . '<span data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="Low In Stock"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-arrow-down-short" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
-                          </svg></span>' . "</td>";
-                        }else{
-                            echo "<td>" . $row['quantity'] . '<span data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="In Stock"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
-                          </svg></span>' . "</td>";
-                            //echo "<td>" . $row['inStock'] . "</td>";
-                        }
+                        echo "<td>" . $row['quantity'] . "</td>";
+                        // if($row['quantity'] < 10){
+                        //     echo "<td>" . $row['quantity'] . '<span data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="Low In Stock"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-arrow-down-short" viewBox="0 0 16 16">
+                        //     <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z"/>
+                        //   </svg></span>' . "</td>";
+                        // }else{
+                        //     echo "<td>" . $row['quantity'] . '<span data-bs-toggle="tooltip" data-bs-placement="left" data-bs-custom-class="tooltip-expired" data-bs-title="In Stock"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" class="bi bi-arrow-up-short" viewBox="0 0 16 16">
+                        //     <path fill-rule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5z"/>
+                        //   </svg></span>' . "</td>";
+                        //     //echo "<td>" . $row['inStock'] . "</td>";
+                        // }
                         echo "<td>" . $row['collectionType'] . "</td>";
                         echo "<td data-sort='". $dataSortExpD ."'>" . $expDateFormatted . "</td>";
                         //echo "<td>" . $row['note'] . "</td>";

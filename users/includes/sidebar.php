b2a7e38032c95568e276f4ef9c69215b7cf4ac0f
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-04-22 22:07:27 +0800

    added sidebar menu for manage users

--- users/includes/sidebar.php
+++ users/includes/sidebar.php
@@ -142,24 +142,13 @@
                     </div>
 
                     <!-- Users -->
-                    <div class="sb-sidenav-menu-heading"> Users</div>
-                    <!-- Medication -->
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#users" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+                    <div class="sb-sidenav-menu-heading">Users</div>
+                    <a class="nav-link <?= $page == 'users.php' || $page == 'register.php' ? 'active' : '' ?>" href="users.php">
+                        <div class="sb-nav-link-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
                         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-                        </svg>
-                        </i>
-                        </div>
-                        Manager Users
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </svg></div>
+                        Manage Users
                     </a>
-                    <div class="collapse" id="users" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="medication_pending.php">Active Users</a>
-                            <a class="nav-link" href="medication_completed.php">Disabled Users</a>
-                        </nav>
-                    </div>
 
                     <!-- Deleted Records -->
                     <div class="sb-sidenav-menu-heading"> Records</div>

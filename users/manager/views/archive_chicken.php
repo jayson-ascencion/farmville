1d03edc588f97de3d0cd859746632bdccf44683a
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-04-22 14:46:46 +0800

    added confirmation before archiving

--- users/manager/views/archive_chicken.php
+++ users/manager/views/archive_chicken.php
@@ -92,14 +92,14 @@
         <li class="breadcrumb-item">
             <a href="./chicken_production.php" class="text-decoration-none">Chicken Production</a>
         </li>
-        <li class="breadcrumb-item active">Delete Chicken Batch</li>
+        <li class="breadcrumb-item active">Archive Chicken Batch</li>
     </ol>
 
     <div class="row justify-content-center mt-2">
         <div class="col-sm-4">
             <div class="card bg-light shadow-lg mb-4 ">
                 <div class="card-header fw-bold p-3" style="background-color: #FFAF1A;">
-                <div class="text-center" style="background-color: #FFAF1A; color: #91452c">Are you sure you want to delete this record?</div> <div>
+                <div class="text-center" style="background-color: #FFAF1A; color: #91452c">Are you sure you want to archive this record?</div> <div>
                             <!-- <a class="small text-white" href="./chicken_production.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-x-lg" viewBox="0 0 16 16">
                             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
@@ -157,7 +157,7 @@
 
                         </div>
                         <input type="hidden" name="id" value="<?php echo $id; ?>"/>
-                        <div class="card-footer w-100 border d-flex justify-content-between">
+                        <!-- <div class="card-footer w-100 border d-flex justify-content-between">
                             <div class="w-100 m-1">
                                 <a class="small btn  btn-outline-secondary fw-bold w-100" href="./chicken_production.php">
                                     Cancel
@@ -167,6 +167,48 @@
                                 <button type="submit" name="archiveRecord" class="btn   btn-outline-danger fw-bold w-100">
                                     Delete
                                 </button>                                    
+                            </div>
+                        </div> -->
+
+                        <div class="card-footer w-100 border d-flex justify-content-between">
+                            <div class="w-100 m-1">
+                                <a class="small btn btn-outline-secondary fw-bold w-100" href="./chicken_production.php">
+                                    Cancel
+                                </a> 
+                            </div>
+                            <div class="w-100 m-1">
+                                <button type="button" name="archiveRecord" class="btn  btn-outline-danger  fw-bold w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                Archive
+                                </button>                                    
+                            </div>
+                        </div>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                    </svg>
+                                    <h1 class="modal-title fs-6 ms-1" id="exampleModalLabel">Archive Record?</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body"> Once archived, only the administrator can restore the record.
+                                </div>
+                                <div class="modal-footer">
+                                    <div class="w-100    d-flex justify-content-between">
+                                        <div class="w-100 m-1">
+                                            <button type="button" class="btn w-100 fw-bold btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+                                        </div>
+                                        <div class="w-100 m-1">
+                                            <button type="submit" name="archiveRecord" class="btn  btn-outline-danger fw-bold w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                            Yes
+                                            </button>                                    
+                                        </div>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                         </div>
                     </form>

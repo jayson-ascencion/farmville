bb834166b5a787d554495fb6d9cb4c31e6850f69
ozonelayer-source <jayson.ascencionx@gmail.com>
2023-04-22 13:54:09 +0800

    restored back to previous version due to commit error

--- users/admin/views/delete_chicken.php
+++ users/admin/views/delete_chicken.php
@@ -47,9 +47,9 @@
             // $sql = "UPDATE chickenproduction, schedules SET archive=:archived WHERE chickenBatch_ID = '$id'";
             // $sql = "UPDATE chickenproduction c, schedules s SET c.archive = :archived, s.archive = :archived WHERE c.chickenBatch_ID = s.chickenBatch_ID AND c.chickenBatch_ID = '$id'";
             $sql = "BEGIN;
-            Archive FROM chickenreduction WHERE chickenBatch_ID = '$id';
-            Archive FROM schedules WHERE chickenBatch_ID = '$id';
-            Archive FROM chickenproduction WHERE chickenBatch_ID = '$id';
+            DELETE FROM chickenreduction WHERE chickenBatch_ID = '$id';
+            DELETE FROM schedules WHERE chickenBatch_ID = '$id';
+            DELETE FROM chickenproduction WHERE chickenBatch_ID = '$id';
             COMMIT;
             ";
 
@@ -64,7 +64,7 @@
                 // Attempt to execute the prepared statement
                 if($stmt->execute())
                 {
-                    $_SESSION['status'] = "Record Successfully Archived";
+                    $_SESSION['status'] = "Record Successfully Deleted";
                     header("Location: chicken_production.php");
                 } 
                 else
@@ -92,14 +92,14 @@
         <li class="breadcrumb-item">
             <a href="./chicken_production.php" class="text-decoration-none">Chicken Production</a>
         </li>
-        <li class="breadcrumb-item active">Archive Chicken Batch</li>
+        <li class="breadcrumb-item active">Delete Chicken Batch</li>
     </ol>
 
     <div class="row justify-content-center mt-2">
         <div class="col-sm-4">
             <div class="card bg-light shadow-lg mb-4 ">
                 <div class="card-header fw-bold p-3" style="background-color: #FFAF1A;">
-                <div class="text-center" style="background-color: #FFAF1A; color: #91452c">Are you sure you want to Archive this record?</div> <div>
+                <div class="text-center" style="background-color: #FFAF1A; color: #91452c">Are you sure you want to delete this record?</div> <div>
                             <!-- <a class="small text-white" href="./chicken_production.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-x-lg" viewBox="0 0 16 16">
                             <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
@@ -160,7 +160,7 @@
                             </div>
                             <div class="w-100 m-1">
                                 <button type="button" name="archiveRecord" class="btn  btn-outline-danger  fw-bold w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                Archive
+                                Delete
                                 </button>                                    
                             </div>
                         </div>
@@ -173,10 +173,10 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                                     </svg>
-                                    <h1 class="modal-title fs-6 ms-1" id="exampleModalLabel">Permanently Archive Record?</h1>
+                                    <h1 class="modal-title fs-6 ms-1" id="exampleModalLabel">Permanently Delete Record?</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <div class="modal-body"> Once Archived, the record cannot be retrieved or restored.
+                                <div class="modal-body"> Once deleted, the record cannot be retrieved or restored.
                                 </div>
                                 <div class="modal-footer">
                                     <div class="w-100    d-flex justify-content-between">
--- users/admin/views/delete_schedules.php
+++ users/admin/views/delete_schedules.php
@@ -1,6 +1,6 @@
 <?php
     //page title
-    $title = "Archive Schedule";
+    $title = "Delete Schedule";
     
     //header
     include('../../includes/header.php');
@@ -48,7 +48,7 @@
 
             // Prepare an insert statement
             $sql = "BEGIN;
-            Archive FROM schedules WHERE administration_ID = '$id';
+            DELETE FROM schedules WHERE administration_ID = '$id';
             COMMIT;
             ";
             
@@ -63,7 +63,7 @@
                 // Attempt to execute the prepared statement
                 if($stmt->execute())
                 {
-                    $_SESSION['status'] = "Schedule Successfully Archived."; 
+                    $_SESSION['status'] = "Schedule Successfully Deleted."; 
                     header("Location: schedules.php");
                 } 
                 else
@@ -91,7 +91,7 @@
         <li class="breadcrumb-item">
             <a class="text-decoration-none" href="./medication_pending.php">Medication Schedules</a>
         </li>
-        <li class="breadcrumb-item active">Archive Schedule</li>
+        <li class="breadcrumb-item active">Delete Schedule</li>
     </ol>
 
 
@@ -186,7 +186,7 @@
                             </div>
                             <div class="w-100 m-1">
                                 <button type="button" name="archiveRecord" class="btn  btn-outline-danger  fw-bold w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                Archive
+                                Delete
                                 </button>                                    
                             </div>
                         </div>
@@ -199,10 +199,10 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="orange" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
                                     <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                                     </svg>
-                                    <h1 class="modal-title fs-6 ms-1" id="exampleModalLabel">Permanently Archive Record?</h1>
+                                    <h1 class="modal-title fs-6 ms-1" id="exampleModalLabel">Permanently Delete Record?</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <div class="modal-body"> Once Archived, the record cannot be retrieved or restored.
+                                <div class="modal-body"> Once deleted, the record cannot be retrieved or restored.
                                 </div>
                                 <div class="modal-footer">
                                     <div class="w-100    d-flex justify-content-between">
